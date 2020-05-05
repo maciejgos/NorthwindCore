addf2029a90e537eb75b43c90ed9c7a30bd74f3a
Maciej Gos <maciej@maciejgos.com>
2020-05-05 18:19:29 +0200

    Add simple products controller

---
+++ src/Northwind.Backoffice.Web/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Backoffice.Web.Application.Handlers;
+
+namespace Northwind.Backoffice.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var request = new GetAllProductsRequest();
+            var response = await _mediator.Send(request);
+
+            return View(response);
+        }
+    }
+}
