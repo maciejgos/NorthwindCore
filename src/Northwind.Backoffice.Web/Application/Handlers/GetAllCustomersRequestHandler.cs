a3b254673a58465f8d3df09c09f22d014cdab024
Maciej Gos <maciej@maciejgos.com>
2020-05-16 19:22:45 +0200

    Add get all customers feature

---
+++ src/Northwind.Backoffice.Web/Application/Handlers/GetAllCustomersRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Northwind.Backoffice.Core.Entities;
+using Northwind.Backoffice.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Northwind.Backoffice.Web.Application.Handlers
+{
+    public class GetAllCustomersRequestHandler : IRequestHandler<GetAllCustomersRequest, IEnumerable<Customers>>
+    {
+        private readonly NorthwindContext _context;
+
+        public GetAllCustomersRequestHandler(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<Customers>> Handle(GetAllCustomersRequest request, CancellationToken cancellationToken)
+        {
+            var customers = _context.Customers.AsEnumerable();
+
+            return Task.FromResult(customers);
+        }
+    }
+
+    public class GetAllCustomersRequest : IRequest<IEnumerable<Customers>>
+    {
+
+    }
+}
