e53855e0b567e0bd7dbcb1252d72de7fd3f45e5d
Maciej Gos <maciej@maciejgos.com>
2020-05-16 19:05:29 +0200

    Add get of all orders

---
+++ src/Northwind.Backoffice.Web/Application/Handlers/GetAllOrdersRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Northwind.Backoffice.Core.Entities;
+using Northwind.Backoffice.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Northwind.Backoffice.Web.Application.Handlers
+{
+    public class GetAllOrdersRequestHandler : IRequestHandler<GetAllOrdersRequest, IEnumerable<Orders>>
+    {
+        private readonly NorthwindContext _context;
+
+        public GetAllOrdersRequestHandler(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<Orders>> Handle(GetAllOrdersRequest request, CancellationToken cancellationToken)
+        {
+            var orders = _context.Orders.AsEnumerable();
+            return Task.FromResult(orders);
+        }
+    }
+
+    public class GetAllOrdersRequest : IRequest<IEnumerable<Orders>>
+    {
+    }
+}
--- src/Northwind.Backoffice.Web/Controllers/OrdersController.cs
+++ src/Northwind.Backoffice.Web/Controllers/OrdersController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Backoffice.Web.Application.Handlers;
 
 namespace Northwind.Backoffice.Web.Controllers
 {
     public class OrdersController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var request = new GetAllOrdersRequest();
+            var response = await _mediator.Send(request);
+
+            return View(response);
         }
     }
 }
