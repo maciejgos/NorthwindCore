c906e9e63c4dc1e80bad8302c0af2141826ddb0d
Maciej Gos <maciej@maciejgos.com>
2020-05-19 17:27:40 +0200

    Update GetAllProductsRequestHandler.cs

--- src/Northwind.Backoffice.Web/Application/Handlers/GetAllProductsRequestHandler.cs
+++ src/Northwind.Backoffice.Web/Application/Handlers/GetAllProductsRequestHandler.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
-using Northwind.Backoffice.Core.Entities;
 using Northwind.Backoffice.Infrastructure.Data;
+using Northwind.Backoffice.Web.Application.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Northwind.Backoffice.Web.Application.Handlers
 {
-    public class GetAllProductsRequestHandler : IRequestHandler<GetAllProductsRequest, IEnumerable<Product>>
+    public class GetAllProductsRequestHandler : IRequestHandler<GetAllProductsRequest, IEnumerable<ProductDto>>
     {
         private readonly NorthwindContext _context;
 
@@ -17,14 +17,14 @@ namespace Northwind.Backoffice.Web.Application.Handlers
             _context = context;
         }
 
-        public Task<IEnumerable<Product>> Handle(GetAllProductsRequest request, CancellationToken cancellationToken)
+        public Task<IEnumerable<ProductDto>> Handle(GetAllProductsRequest request, CancellationToken cancellationToken)
         {
-            var products = _context.Products.AsEnumerable();
+            var products = _context.Products.Select(p => new ProductDto(p)).AsEnumerable();
             return Task.FromResult(products);
         }
     }
 
-    public class GetAllProductsRequest : IRequest<IEnumerable<Product>>
+    public class GetAllProductsRequest : IRequest<IEnumerable<ProductDto>>
     {
     }
 }
