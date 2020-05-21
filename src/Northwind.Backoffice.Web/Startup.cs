6f1477797673f447815f64c91b479a420e2c845f
Maciej Gos <maciej@maciejgos.com>
2020-05-21 08:10:56 +0200

    Replaced by API project

--- src/Northwind.Backoffice.Web/Startup.cs
+++ src/Northwind.Backoffice.Web/Startup.cs
@@ -1,21 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Northwind.Backoffice.Infrastructure;
-using Northwind.Backoffice.Infrastructure.Data;
-using Northwind.Backoffice.Web.Application;
 using Northwind.Backoffice.Web.Extensions;
 
 namespace Northwind.Backoffice.Web
 {
     public class Startup
     {
-        private const string ConnectionStringName = "DefaultConnection";
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,20 +19,10 @@ namespace Northwind.Backoffice.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddInfrastructure(Configuration.GetConnectionString(ConnectionStringName));
-            services.AddApplication();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
-
             services.AddRazorPages().AddRazorPagesOptions(options =>
             {
                 options.RootDirectory = "/Views";
             });
-
-            services.AddHealthChecks()
-                    .AddCheck("live", () => HealthCheckResult.Unhealthy("Application is not responding"))
-                    .AddDbContextCheck<NorthwindContext>("db", HealthStatus.Degraded, new[] { "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,25 +44,9 @@ namespace Northwind.Backoffice.Web
 
             app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseRequestResponseLogging();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
-                endpoints.MapHealthChecks("/health/ready", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
-                {
-                    Predicate = (check) => check.Tags.Contains("ready")
-                });
-                endpoints.MapHealthChecks("/health/live", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
-                {
-                    Predicate = _ => false
-                });
             });
         }
     }
